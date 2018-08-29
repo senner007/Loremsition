281ea583b8d0c3264de7b953d864c2f1a7e089b3
senner007 <nielshtg@hotmail.com>
2018-08-30 00:29:59 +0200

    dynamically import html,js polyfill ie

--- index.js
+++ index.js
@@ -1,84 +1,31 @@
-import $ from 'jquery';
-import {vertical} from "./examples/vertical-binary/vertical-binary.js"
-import {horizontal } from "./examples/horizontal-binary/horizontal-binary.js"
 import Loremsition from './src/main.js'
-import {resizeThrottle, liInc, romanize} from './examples/utils/utils_helpers'
-
-import './css/vertical-binary.css';
-import './css/horizontal.css';
 import './css/sortCss.css';
+import './css/horizontal.css';
+import './css/vertical-binary.css';
 
-//$('.container').hide(); // cannot be set from css ???
+import "babel-polyfill";
+import 'whatwg-fetch'
 
 (function () {
     var path = window.location.pathname.split('/');
 
     if (path[2] == 'horizontal') {
-        $('.grid-vertical').remove();
-        var containers = horizontal(Loremsition);
+      var assets = {
+        html : import('./examples/horizontal-unary/horizontal-unary.html'),
+        js : import("./examples/horizontal-unary/horizontal-unary.js")
+      }
+          
     }
     else  {
-        $('.grid-horizontal').remove();
-        var containers = vertical(Loremsition);
+      var assets = {
+        html : import('./examples/vertical-binary/vertical-binary.html'),
+        js : import("./examples/vertical-binary/vertical-binary.js")
+      }
     } 
 
-    // containers[0].lock();
-
-    
-      containers[1].div.addEventListener('onLayoutAll', function () {
-        containers[1].removeLiElem(1, true, function () {
-          containers[1].addLiElem("<span class='special'>E</span>element added after remove animation", 0)
-         })
-     })
-
-     loopContainers(function (c) {
-      c.div.addEventListener('onDropTo', function (ev) {
-
-       // loopContainers(function(v) { v.reCalculate()})
-      })
-    })
-  
-    function loopContainers(fn) {
-      containers.forEach(fn)
-    }
-  
-  
-      loopContainers(function(v) { v.init()})
-
-      document.body.querySelector('.container').style.visibility = "visible";
-   
-    
-  
-    resizeThrottle(function () {
-      console.log('resizing!')
-      loopContainers(function(v) { v.setCutOff(path[1] == 'horizontal' ? window.innerWidth : window.innerHeight)})
-  
-      // example of using the cutOffEnd method on the object's prototype.
-      //Here, upon resize, it cuts the list when height is above specified value and prepends to adjacent container
-  
-      loopContainers(function(v) { v.cutOffEnd()})
-      
-      loopContainers(function(v) { v.reLayout()})
-  
-    })
-
-}());
-
-(function pageSetup() {
-
-    $.fn.disableSelection = function() {
-      return this
-        .attr('unselectable', 'on')
-        .css('user-select', 'none')
-        .on('selectstart', false);
-    };
-  
-    //$("body").css("overflow", "hidden");
-    $('body').disableSelection();
-    $('ul').on('touchmove', function(e) {
-      e.preventDefault();
+    Promise.all([assets.html, assets.js]).then(function(values) {
+      document.querySelector('.container').innerHTML = values[0];
+      values[1]['default'](Loremsition)
     });
-  
+
 }());
-
-
