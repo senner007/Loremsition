7a6cb83421fd8dd0e98f698fabfc11e54a4fb7c5
senner007 <nielshtg@hotmail.com>
2018-09-03 00:11:33 +0200

    migrate to webpack

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
 import Loremsition from './src/main.js'
 import './css/sortCss.css';
 
-import './examples/horizontal-unary/horizontal-unary.css';
-import './examples/vertical-binary/vertical-binary.css';
-import './examples/vertical-n-ary/vertical-n-ary.css';
+// import './examples/horizontal-unary/horizontal-unary.css';
+// import './examples/vertical-binary/vertical-binary.css';
+// import './examples/vertical-n-ary/vertical-n-ary.css';
 
 import 'core-js/features/promise';  
 import 'whatwg-fetch';
 
-(function () {
-    var path = window.location.pathname.split('/');
 
-    if (path[2] == 'horizontal-unary') {
+(function () {
+    var path = window.location.pathname.split('/').join('');
+    var myRe = /((vertical|horizontal)[-\w]+)/
+    var test = myRe.exec(path)[0];
+
+    if (test) {
       var assets = {
-        html : import('./examples/horizontal-unary/horizontal-unary.html'),
-        js : import("./examples/horizontal-unary/horizontal-unary.js")
+        css : import(`./examples/${test}/${test}.css`),
+        html : import(`./examples/${test}/${test}.html`),
+        js : import(`./examples/${test}/${test}.js`)
       }      
     }
-    else if (path[2] == 'vertical-binary') {
-      var assets = {
-        html : import('./examples/vertical-binary/vertical-binary.html'),
-        js : import("./examples/vertical-binary/vertical-binary.js")
-      }
-    }
-    else if (path[2] == 'vertical-n-ary' || path[2] == "" || !path[2]) {
-      var assets = {
-        html : import('./examples/vertical-n-ary/vertical-n-ary.html'),
-        js : import("./examples/vertical-n-ary/vertical-n-ary.js")
-      }
-    }  
+    // else if (path.includes('vertical-binary')) {
+    //   var assets = {
+    //     html : import('./examples/vertical-binary/vertical-binary.html'),
+    //     js : import("./examples/vertical-binary/vertical-binary.js")
+    //   }
+    // }
+    // else if (path.includes('vertical-n-ary') || path[2] == "" || !path[2]) {
+    //   var assets = {
+    //     html : import('./examples/vertical-n-ary/vertical-n-ary.html'),
+    //     js : import("./examples/vertical-n-ary/vertical-n-ary.js")
+    //   }
+    // }  
 
-    Promise.all([assets.html, assets.js]).then(function(values) {
-      document.querySelector('.container').innerHTML = values[0];
-      values[1]['default'](Loremsition)
+    Promise.all([assets.css, assets.html, assets.js]).then(function(values) {
+      document.querySelector('.container').innerHTML = values[1].default;
+      values[2]['default'](Loremsition)
     });
 
 }());
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,69 @@
-var webpack = require("webpack");
-
-const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
 
 module.exports = {
-  // mode: 'none',
- 
-  entry: './src/main.js',
+  entry: {
+    'library/loremsition': './src/main.js',
+    'examples': './index.js'
+  },
+  mode: "production",
+  devServer: {
+    historyApiFallback: true,
+        contentBase: './'
+   },
   output: {
-    library: 'Loremsition', // set as global var
-    filename: 'loremsition.js',
-    path: path.resolve(__dirname, 'prod')
+    path: path.resolve(__dirname, './prod'),
+    filename: '[name].js',
   },
   module: {
     rules: [
-      {
+      { 
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
           options: {
-            babelrc: false, // don't look in .babelrc file ( used by parcel and jest )
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env'],
+            plugins: ["syntax-dynamic-import"]
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            attrs: [':data-src']
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    // new HtmlWebpackPlugin({
+    //   template : "./index.html",
+    //   chunks: ['app'],
+    //   filename: 'index.html'
+    // }),
+    new HtmlWebpackPlugin({
+      template : "./index.html",
+      chunks: ['examples'],
+      filename: 'index.html'
+    })
+  ]
 };
-
