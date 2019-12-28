1cddfd309ff03f4821571af23553d6cf6c132b67
nig <nig@beepanalytics.com>
2019-12-28 19:52:24 +0100

    wip

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ import 'whatwg-fetch';
 
 (function () {
     var path = window.location.pathname.split('/').join('');
-    console.log(path)
     var myRe = /((vertical|horizontal)[-\w]+)/
     var test = myRe.exec(path)[0];
 
@@ -21,19 +20,7 @@ import 'whatwg-fetch';
         html : import(`./examples/${test}/${test}.html`),
         js : import(`./examples/${test}/${test}.js`)
       }      
-    }
-    // else if (path.includes('vertical-binary')) {
-    //   var assets = {
-    //     html : import('./examples/vertical-binary/vertical-binary.html'),
-    //     js : import("./examples/vertical-binary/vertical-binary.js")
-    //   }
-    // }
-    // else if (path.includes('vertical-n-ary') || path[2] == "" || !path[2]) {
-    //   var assets = {
-    //     html : import('./examples/vertical-n-ary/vertical-n-ary.html'),
-    //     js : import("./examples/vertical-n-ary/vertical-n-ary.js")
-    //   }
-    // }  
+    } 
 
     Promise.all([assets.css, assets.html, assets.js]).then(function(values) {
       document.querySelector('.container').innerHTML = values[1].default;
